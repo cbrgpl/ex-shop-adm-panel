444868c78449fdd53e16ba031501ed65a8299e0d
Dmitry <cybirgpl@gmail.com>
2022-07-04 22:50:22 +0700

    feat(card): сделал компонент карточки;

---
+++ src/components/general/atomic/ZCard.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="card">
+    <slot />
+
+    <Transition name="card__loader-transition">
+      <div
+        v-if="loader"
+        class="card__loader-wrapper"
+      >
+        <ZLoader
+          size="md"
+        />
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<script>
+import ZLoader from '@general_components/atomic/ZLoader.vue'
+
+export default {
+  name: 'ZCard',
+  expose: ['setLoaderStateTo'],
+  components: {
+    ZLoader
+  },
+  data() {
+    return {
+      loader: false,
+    }
+  },
+  methods: {
+    setLoaderStateTo(  loaderState ) {
+      this.loader = loaderState
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+	position: relative;
+	overflow: hidden;
+	border-radius: 4px;
+	background: $surface;
+	box-shadow: 0 20px 30px rgb(0 0 0 / 0.04), 0 6px 10px rgb(0 0 0 / 0.02);
+
+	&__loader-wrapper {
+		position: absolute;
+		top: 0;
+		left: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+		background: rgb(0 0 0 / 0.35);
+	}
+}
+
+.card__loader-transition {
+	&-enter-from,
+	&-leave-to {
+		opacity: 0;
+	}
+
+	&-enter-active {
+		transition: all 140ms;
+	}
+
+	&-leave-active {
+		transition: all 80ms;
+	}
+}
+</style>
--- src/views/TheCatalog.vue
+++ src/views/TheCatalog.vue
@@ -29,8 +29,12 @@
     >
       Select an item
     </ZButtonWithLoader>
-
-    <ZProductForm @submitted="test" />
+    <ZCard
+      ref="card"
+      style="width: 50%; margin: auto; padding: 24px;"
+    >
+      <ZProductForm @submitted="test" />
+    </ZCard>
 
     <div style="width: 420px;">
       <ZRequiredInput
@@ -51,6 +55,7 @@ import ZButton from '@general_components/atomic/ZButton.vue'
 import ZButtonWithLoader from '@general_components/composite/ZButtonWithLoader.vue'
 import ZProductForm from '@admin_components/composite/ZProductForm.vue'
 import ZRequiredInput from '@general_components/composite/ZRequiredInput.vue'
+import ZCard from '@general_components/atomic/ZCard.vue'
 
 export default {
   name: 'TheCatalog',
@@ -60,12 +65,14 @@ export default {
     ZButton,
     ZButtonWithLoader,
     ZProductForm,
-    ZRequiredInput
+    ZRequiredInput,
+    ZCard
   },
   data() {
     return {
       text: 'zxc',
       inputError: null,
+      loader: false
     }
   },
   methods: {
@@ -73,6 +80,8 @@ export default {
       this.inputError = !this.inputError
     },
     test(ev) {
+      this.$refs.card.setLoaderStateTo(!this.loader)
+      this.loader = !this.loader
       console.log('test', ev)
     }
   },
